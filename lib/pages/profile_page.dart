fc06de0adbf75f810380de897332a32e8d147508
NeriyaZudi <neriyazudi@gmail.com>
2023-05-08 00:18:25 +0300

    start profile page

--- lib/pages/profile_page.dart
+++ lib/pages/profile_page.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:lottie/lottie.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:smartStoveApp/auth/auth_service.dart';
-import 'package:smartStoveApp/components/navigation_bar.dart';
 import 'package:smartStoveApp/constants/routes.dart';
 import 'package:smartStoveApp/utilities/show_logout_dialog.dart';
 
 class ProfilePage extends StatelessWidget {
   const ProfilePage({super.key});
+  final double coverHeight = 250;
+  final double profileHeight = 144;
 
   @override
   Widget build(BuildContext context) {
+    final top = coverHeight - profileHeight / 2;
+    final bottom = profileHeight / 2;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: (Colors.grey[300])!,
@@ -48,6 +51,88 @@ class ProfilePage extends StatelessWidget {
           )
         ],
       ),
+      body: ListView(
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          buildTopProfile(top, bottom),
+          buildContent(),
+        ],
+      ),
     );
   }
+
+  Widget buildTopProfile(double top, double bottom) {
+    return Stack(
+        clipBehavior: Clip.none,
+        alignment: Alignment.center,
+        children: [
+          Container(
+              margin: EdgeInsets.only(bottom: bottom),
+              child: buildCoverImage()),
+          Positioned(
+            top: top,
+            child: buildProfileImage(),
+          )
+        ]);
+  }
+
+  Widget buildCoverImage() => Container(
+        color: Colors.grey[400],
+        child: Image.asset(
+          'assets/images/cover.png',
+          width: double.infinity,
+          height: coverHeight,
+          fit: BoxFit.cover,
+        ),
+      );
+  Widget buildProfileImage() => CircleAvatar(
+        backgroundColor: Colors.grey.shade700,
+        backgroundImage: const AssetImage('lib/images/eilon.jpg'),
+        radius: profileHeight / 2,
+      );
+  Widget buildContent() {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 48),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          const Text(
+            'Eilon Yifrach',
+            style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 15),
+          const Text(
+            'Electricity consumption when using stoves',
+            style: TextStyle(fontSize: 10),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const SizedBox(height: 12),
+              buildSocialIcon(FontAwesomeIcons.github),
+              const SizedBox(height: 12),
+              buildSocialIcon(FontAwesomeIcons.linkedin),
+              const SizedBox(height: 12),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildSocialIcon(IconData icon) => CircleAvatar(
+      radius: 25,
+      child: Material(
+        shape: const CircleBorder(),
+        clipBehavior: Clip.hardEdge,
+        color: Colors.transparent,
+        child: InkWell(
+          onTap: () {
+            print(icon.toString());
+          },
+          child: Center(
+            child: Icon(icon, size: 32),
+          ),
+        ),
+      ));
 }
